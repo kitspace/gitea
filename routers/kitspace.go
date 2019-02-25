c2d5d32d9ffc944637ce075adc8803b162cf5f3c
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-02-25 00:13:05 +0000

    Add sub-url replacement to kitspace route

--- routers/kitspace.go
+++ routers/kitspace.go
@@ -2,15 +2,19 @@ package routers
 
 import (
 	"bytes"
-	"code.gitea.io/gitea/models"
-	"code.gitea.io/gitea/modules/context"
 	"encoding/json"
 	"fmt"
-	"github.com/go-macaron/csrf"
-	"github.com/go-macaron/session"
 	"io/ioutil"
 	"net/http"
+	"path"
 	"strings"
+
+	"github.com/go-macaron/csrf"
+	"github.com/go-macaron/session"
+
+	"code.gitea.io/gitea/models"
+	"code.gitea.io/gitea/modules/context"
+	"code.gitea.io/gitea/modules/setting"
 )
 
 type Message struct {
@@ -20,12 +24,19 @@ type Message struct {
 }
 
 func Kitspace(ctx *context.Context, sess session.Store, x csrf.CSRF) []byte {
-	m := Message{User: ctx.User, Csrf: x.GetToken(), Route: strings.Replace(ctx.Link, "/kitspace", "", 1)}
-	fmt.Printf("%+v\n", ctx)
+	m := Message{
+		User: ctx.User,
+		Csrf: x.GetToken(),
+		Route: strings.Replace(
+			ctx.Link,
+			path.Join(setting.AppSubURL, "/kitspace"), "", 1,
+		),
+	}
 	b, err := json.Marshal(m)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(string(b))
 	url := "http://localhost:3001/"
 	resp, err := http.Post(url, "application/json", bytes.NewBuffer(b))
 	if err != nil {
